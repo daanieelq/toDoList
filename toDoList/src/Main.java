738aea58e7f778c2a5983bb7b9be0df977bf6b36
daanieelq <150684428+daanieelq@users.noreply.github.com>
2023-11-21 16:13:56 +0100

    Add files via upload

---
+++ toDoList/src/Main.java
@@ -0,0 +1,12 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        String eingabe = sc.nextLine();
+
+        System.out.println("Die Liste für die heutigen Aufgaben lautet: " + eingabe);
+
+    }
+}
